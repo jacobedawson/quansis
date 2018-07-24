fdf296c9b4435acf673a8074419328949478699e
Jacob Edward Dawson <reviber8@gmail.com>
2018-07-24 09:13:29 +0200

    added basic contact form

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import Scroll from 'scroll-js';
 
-const scrollButtons = [...document.querySelectorAll('[data-scrollto]')];
+const pageSelectButtons = [...document.querySelectorAll('[data-scrollto]')];
 
-scrollButtons.forEach(el => {
+pageSelectButtons.forEach(el => {
     const target = document.getElementById(`${el.dataset.scrollto}`);
     el.addEventListener('click', () => {
         const scroll = new Scroll(document.documentElement.scrollTop > 0 ? document.documentElement : document.body);
         scroll.toElement(target, {
             easing: 'easeInOutCubic',
             duration: 750
-        }).then(bodyClassToggle(el.dataset.scrollto));
+        })
+        .then(bodyClassToggle(el.dataset.scrollto))
+        .then(toggleSelectedMenu(el));
     });
 });
 
+// Call this on scroll as well to match scroll position
+function toggleSelectedMenu(el) {
+    pageSelectButtons.forEach(button => button.classList.remove('selected'));
+    el.classList.add('selected');
+}
+
 const footer = document.querySelector('footer');
+const nav = document.querySelector('nav');
 function bodyClassToggle(name) {
     if (name.match(/^page-(3|4|6|7|9|10)$/)) {
+        nav.classList.add('dark'); 
         footer.classList.add('dark');
     } else {
+        nav.classList.remove('dark');
         footer.classList.remove('dark');
     }
 }
+
