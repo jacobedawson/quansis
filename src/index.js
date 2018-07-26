d65ff33ff2f09e0aae05c025123e055a8dd1ddfe
Jacob Edward Dawson <reviber8@gmail.com>
2018-07-26 23:32:59 +0200

    added pure js modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Scroll from 'scroll-js';
-const scrollMonitor = require('scrollmonitor');
+import scrollMonitor from 'scrollmonitor';
+const modals = require('./js/modals');
+
 
 const pageSelectButtons = [...document.querySelectorAll('[data-scrollto]')];
 
@@ -24,13 +26,33 @@ function toggleSelectedMenu(el) {
 
 const footer = document.querySelector('footer');
 const nav = document.querySelector('nav');
+const scrollMore = document.getElementById('scroll-more');
 function bodyClassToggle(name) {
     if (name.match(/^page-(3|4|6|7|9|x)$/)) {
         nav.classList.add('dark'); 
         footer.classList.add('dark');
+        scrollMore.classList.add('dark');
     } else {
         nav.classList.remove('dark');
         footer.classList.remove('dark');
+        scrollMore.classList.remove('dark');
+    }
+}
+
+function hideElements(name) {
+    // hide the page select on page 1
+    if (name.includes('page-1')) {
+        // hide the page select
+        document.getElementById('page-select').style.display = 'none';
+    } else {
+        document.getElementById('page-select').style.display = 'flex';
+    }
+    // hide the scroll more on page 10
+    if (name.includes('page-x')) {
+        // hide the scroll more
+        document.getElementById('scroll-more').style.display = 'none';
+    } else {
+        document.getElementById('scroll-more').style.display = 'flex';
     }
 }
 
@@ -45,6 +67,7 @@ spans.forEach(s => {
         const menuItem = document.querySelector('[data-scrollto=' + `${activeID}` + ']');
         toggleSelectedMenu(menuItem);
         bodyClassToggle(activeID);
+        hideElements(activeID);
     });
 })
 
---
+++ src/js/modals.js
@@ -0,0 +1,16 @@
+import tingle from 'tingle.js';
+
+const modal_1 = new tingle.modal({
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: 'Close',
+    cssClass: ['q-modal']
+});
+
+modal_1.setContent(`
+<h1>Unternehmen</h1>
+<p>Die  ständige  und  umfassende  Weiterbildung  unserer  engagierten  Mitarbeiter  ist  eine  Selbstverständlichkeit.  Dadurch  können  wir  Ihnen  Consulting-Leistungen  auf  höchstem  Niveau  bieten.  Münzen  Sie  unser  Know-How  in  Ihren  Wettbewerbsvorteil  um!</p>
+<p>Als  Oracle-Partner  sind  wir  zudem  stets  auf  dem  neuesten  Stand  der  Technologie  aus  dem  Hause  Oracle.  Wir  arbeiten  mit  den  neuesten  Features  der  Oracle  11g  Datenbank  unter  Linux,  Solaris  und  Windows,  implementieren  Cluster  und  RAC-Lösungen,  11g  Application-Server  und  Business  Intelligence-Lösungen.</p>`);
+
+document.getElementById('footer-1').addEventListener('click', () => {
+    modal_1.open();
+});
